1f4b0cbc47b96d0db26050f5d2bb52b106a9986d
Christophe Saugé <csauge@gmail.com>
2024-01-29 17:15:42 +0100

    Incorrect management of some addresses (about 5%)
    - Move the all parsing of addresses to readJson.py:
      - This can be reusable to all other billetteries (new issue to open)
      - Thus we can remove the former get_address_data and strip_zip_code
    - Enhance logic for address:
      - 40 events are newly added (they were ignored until now)
      - 10 incorrect events are removed (mainly international events)
      - 10 differents addresses are more accurate
      - Remove the usage of str.title() which lead to incorrect cases

--- utils/readJson.py
+++ utils/readJson.py
@@ -1,5 +1,80 @@
+import csv
 import re
 import requests
+
+from io import StringIO
+
+
+def get_french_address(location, address, postcode, city):
+    full_address = f"{address} {postcode} {city}"
+    url = "https://api-adresse.data.gouv.fr/search"
+    params = {"q": full_address, "limit": 1}
+    response = requests.get(url, params=params)
+    if data:= response.json():
+        return {
+            "location_name": location,
+            "address": data["features"][0]["properties"]["name"],
+            "city": data["features"][0]["properties"]["city"],
+            "departement": data["features"][0]["properties"]["context"].split(',')[0].strip(),
+            "zip_code": data["features"][0]["properties"]["postcode"],
+            "latitude": data["features"][0]["geometry"]["coordinates"][1],
+            "longitude": data["features"][0]["geometry"]["coordinates"][0],
+        }
+    return {
+        "location_name": "",
+        "address": "",
+        "city": "",
+        "department": "",
+        "zip_code": "",
+        "latitude": "",
+        "longitude": "",
+    }
+
+
+def get_default_address(full_address):
+    full_address = full_address.replace(",", " ").replace('  ', ' ').strip()  # for csv
+    words = full_address.split()
+
+    # create a list of address removing one word for each line until three words minimum
+    if lines := "\n".join(' '.join(words[i:]) for i in range(len(words)) if len(words[i:]) >= 3):
+        url = "https://api-adresse.data.gouv.fr/search/csv/"
+        files = {'data': StringIO("query\n" + lines)}
+        response = requests.post(url, files=files)
+        if data_list := list(csv.DictReader(StringIO(response.text))):
+            data_list = list(map(lambda x: {**x, 'result_score': float(x['result_score']) if x['result_score'] != '' else 0.0}, data_list))
+            max_data = max(data_list, key=lambda x:x['result_score'])
+            if max_data["result_score"] > 0.5:  # keep only good results
+                return {
+                    "location_name": full_address.removesuffix(max_data["query"]).rstrip("- "),
+                    "address": max_data["result_name"],
+                    "city": max_data["result_city"],
+                    "department": max_data["result_context"].split(',')[0].strip(),
+                    "zip_code": max_data["result_postcode"],
+                    "latitude": float(max_data["latitude"]),
+                    "longitude": float(max_data["longitude"]),
+                }
+    return {
+        "location_name": "",
+        "address": "",
+        "city": "",
+        "department": "",
+        "zip_code": "",
+        "latitude": "",
+        "longitude": "",
+    }
+
+def get_address(full_location):
+    if match := re.match(r"(?P<location>.+), (?P<street>[^,]+), ((?P<postcode>\d{2} ?\d{3}) )?(?P<city>[^,]+), France$", full_location):
+        postcode = match.group('postcode') if match.group('postcode') else ''
+        return get_french_address(match.group("location"), match.group("street"), postcode, match.group("city"))
+    elif match := re.match(r"(?P<street>[^,]+), ((?P<postcode>\d{5}) )?(?P<city>[^,]+), France$", full_location):
+        postcode = match.group('postcode') if match.group('postcode') else ''
+        return get_french_address("", match.group("street"), postcode, match.group("city"))
+    elif match := re.match(r"(?P<location>.+), (?P<street>[^,]+),? (?P<postcode>\d{2} ?\d{3}) (?P<city>[^,]+)$", full_location):
+        return get_french_address(match.group("location"), match.group("street"), match.group('postcode'), match.group("city"))
+    elif match := re.match(r"(?P<street>[^,]+),? (?P<postcode>\d{5}) (?P<city>[^,]+)$", full_location):
+        return get_french_address("", match.group("street"), match.group('postcode'), match.group("city"))
+    return get_default_address(full_location)
 
 
 def get_address_data(text_address):
